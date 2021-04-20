64fe35606818f8e4f1266f57a6b65bc015287ff1
unknown <burakcankazim@gmail.com>
2021-04-20 17:17:03 +0300

    server finished

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
 const helmet = require('helmet');
+const cors = require('cors');
 
 require('dotenv').config();
 
@@ -20,6 +21,10 @@ app.use(morgan('dev'));
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: 'http://localhost:8080',
+}));
 
 require('./models/Entry');
 require('./models/User');
